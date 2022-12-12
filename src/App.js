f323fd5a233c21e259294f477af85c97ac271c3b
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-12-12 17:14:05 +0000

    Add: created a header and setup routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
-import * as React from 'react';
-
-
-import Button from '@mui/material/Button';
+import React from 'react';
+import NavBar from './components/layout/NavBar';
+import styles from './App.module.css'
+import Container from 'react-bootstrap/Container'
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
 
 
-<div>
-      <Button variant="contained">Hello World</Button>
+<div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path='/' render={() =>  <h1>Home Page</h1>} />
+          <Route exact path='/signin' render={() =>  <h1>Sign In</h1>} />
+          <Route exact path='/signup' render={() =>  <h1>Sign Up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
 
   );
