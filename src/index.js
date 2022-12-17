42982461620dcfbb16892ebd1fae28f42a745914
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-12-17 15:13:17 +0000

    Add: custom context for logged in user

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
