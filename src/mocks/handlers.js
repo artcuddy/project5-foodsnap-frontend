9949ff9467b148cd8010804e50e9ff4dac3cca19
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-12-20 11:50:10 +0000

    Add: testing NavBar with mocks

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+
+const baseURL = "https://foodsnap-api.herokuapp.com/"
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "dave",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/artcuddy/image/upload/v1/media/../default-user-profile_ximu05",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
